12fb80983dadd5e440bdf8d228b06e16aca1e74a
dudoser <frielender@gmail.com>
2019-04-04 18:07:46 +0700

    Добавил метод Gaame.getArr, который возвращает матрицу игрового поля. Все тесты проходят.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,6 +3,7 @@ public class Game {
 
     private int size;
 
+    private int [][] arr;
 
     public int add(int firstNum, int secondNum) {
         // todo
@@ -18,13 +19,24 @@ public class Game {
 
     public Game(int size) {
         this.size = size;
+        this.initialize();
     }
 
     public Game() {
         this.size = 4;
+        this.initialize();
+    }
+
+    private void initialize(){
+        this.arr = new int[this.size][this.size];
     }
 
     public int getSize() {
         return this.size;
+    }
+
+
+    public int[][] getArr() {
+        return this.arr;
     }
 }
